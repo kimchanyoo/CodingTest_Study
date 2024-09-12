3392c1775bc3087518141f3a85904eaf1d84dd13
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-09-12 17:07:59 +0900

    [Silver II] Title: 좌표 압축, Time: 1824 ms, Memory: 273412 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(map.containsKey(sorted[i])) {
+                continue;
+            }
+            map.put(sorted[i], map.getOrDefault(sorted[i], count));
+            count++;
+        }
+
+        for(int i = 0; i < n; i++){
+            sb.append(map.get(arr[i])).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
