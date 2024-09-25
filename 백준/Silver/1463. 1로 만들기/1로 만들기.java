3507f36bca58d83c6833e9c66b022fe1e359ba0c
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-09-25 18:53:10 +0900

    [Silver III] Title: 1로 만들기, Time: 188 ms, Memory: 56576 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
     static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,8 +10,6 @@ public class Main{
         int n = Integer.parseInt(br.readLine());
 
         dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 0;
 
         System.out.println(solution(n));
     }
