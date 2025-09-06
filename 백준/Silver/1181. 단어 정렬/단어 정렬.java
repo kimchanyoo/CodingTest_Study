fed9c6f6ff8b317512843f2ed6c4ac74bcc361ec
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2025-09-06 11:38:04 +0900

    [Silver V] Title: 단어 정렬, Time: 296 ms, Memory: 25316 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,36 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
+        int N = Integer.parseInt(br.readLine());
+        Comparator<String> cmp = (a, b) -> {
+            int d = a.length() - b.length();
+            return (d != 0) ? d : a.compareTo(b);
+        };
 
-        for(int i = 0; i < n; i++){
-            set.add(br.readLine());
+        // 정렬 + 중복제거 동시
+        Set<String> set = new TreeSet<>(cmp);
+        for (int i = 0; i < N; i++) set.add(br.readLine());
 
-        }
-
-        String[] list = set.toArray(new String[set.size()]);
-
-        Arrays.sort(list, (o1, o2) -> {
-            if(o1.length() > o2.length()){
-                return 1;
-            }else if(o1.length() < o2.length()){
-                return -1;
-            }else{
-                return o1.compareTo(o2);
-            }
-        });
-
-        for (String s : list) {
-            System.out.println(s);
-        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : set) sb.append(s).append('\n');
+        System.out.print(sb);
     }
 }
